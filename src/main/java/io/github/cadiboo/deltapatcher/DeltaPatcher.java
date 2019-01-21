8b7748cb0abddb568bdec7fcea8cc0557e8c3e4c
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-01-21 19:19:22 +1100

    borked

--- src/main/java/io/github/cadiboo/deltapatcher/DeltaPatcher.java
+++ src/main/java/io/github/cadiboo/deltapatcher/DeltaPatcher.java
@@ -1,9 +1,11 @@
 package io.github.cadiboo.deltapatcher;
 
+import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
 import com.google.gson.Gson;
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.repackage.com.nothome.delta.Delta;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,11 +19,17 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
+import static java.nio.file.StandardOpenOption.READ;
 import static net.minecraftforge.common.ForgeVersion.CheckResult;
 
 /**
@@ -31,15 +39,85 @@ import static net.minecraftforge.common.ForgeVersion.CheckResult;
 public class DeltaPatcher {
 
 	private static final Logger LOGGER = LogManager.getLogger("deltapatcher");
-	static {
-		try {
-			process("deltapatcher", "0.1.0", "https://raw.githubusercontent.com/Cadiboo/Delta-Patcher/master/patches");
-		} catch (IOException e) {
-			e.printStackTrace();
+//	static {
+//		try {
+//			process("deltapatcher", "0.1.0", "https://raw.githubusercontent.com/Cadiboo/Delta-Patcher/master/patches");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	public static void main(String... args) throws Exception {
+//		Delta.main(args);
+
+		final ZipFile source = new ZipFile("/Users/Cadiboo/Desktop/diffs/RenderChunk-rebuildChunk-Hooks-1.12.2-0.3.0-pre7.jar");
+		final ZipFile target = new ZipFile("/Users/Cadiboo/Desktop/diffs/RenderChunk-rebuildChunk-Hooks-1.12.2-0.3.0-pre6.jar");
+		final File generatedDiff = Paths.get("/Users/Cadiboo/Desktop/diffs/RenderChunk-rebuildChunk-Hooks-1.12.2-0.3.0-pre6to1.12.2-0.3.0-pre7.zip").toFile();
+
+		final ZipInputStream sourceZipInputStream = new ZipInputStream(Files.newInputStream(source., READ));
+		final ZipInputStream targetZipInputStream = new ZipInputStream(Files.newInputStream(target.toPath(), READ));
+		generatedDiff.getParentFile().mkdirs();
+		final ZipInputStream generatedDiffZipInputStream = new ZipInputStream(Files.newInputStream(generatedDiff.toPath(), READ));
+
+		final HashSet<String> targetFileNames = Sets.newHashSet();
+		final HashSet<ZipEntry> targetZipEntries = Sets.newHashSet();
+		{
+			ZipEntry zipEntry;
+			while ((zipEntry = targetZipInputStream.getNextEntry()) != null) {
+				if (zipEntry.isDirectory()) {
+					continue;
+				}
+				targetFileNames.add(zipEntry.getName());
+				targetZipEntries.add((ZipEntry) zipEntry.clone());
+			}
 		}
+
+		ZipEntry zipEntry;
+		while ((zipEntry = sourceZipInputStream.getNextEntry()) != null) {
+			if (zipEntry.isDirectory()) {
+				continue;
+			}
+			System.out.println(zipEntry);
+			System.out.println(targetFileNames.contains(zipEntry.getName()));
+
+			args = new String[]{
+					source.getI,
+					Paths.get(target.getName(), zipEntry.getName()).toString(),
+					Paths.get(generatedDiff.getName(), zipEntry.getName()).toString(),
+			};
+
+			Delta.main(args);
+
+		}
+
+		sourceZipInputStream.close();
+		targetZipInputStream.close();
+
+		System.exit(0);
+
+		args = new String[]{
+				"/Users/Cadiboo/Desktop/diffs/RenderChunk-rebuildChunk-Hooks-1.12.2-0.3.0-pre7.jar",
+				"/Users/Cadiboo/Desktop/diffs/RenderChunk-rebuildChunk-Hooks-1.12.2-0.3.0-pre6.jar",
+				"/Users/Cadiboo/Desktop/diffs/RenderChunk-rebuildChunk-Hooks-1.12.2-0.3.0-pre6to1.12.2-0.3.0-pre7.zip",
+		};
+
+		Delta.main(args);
 	}
 
 	private static final int MAX_HTTP_REDIRECTS = 20;
+
+	public static void genPatch(File source, File target) {
+		String[] args = {
+				"-d",
+				source.getAbsolutePath(),
+				target.getAbsolutePath()
+		};
+		try {
+			Delta.main(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	//find current version of mod
 	//find next version, next, next, next
@@ -80,8 +158,7 @@ public class DeltaPatcher {
 		@SuppressWarnings("unchecked")
 		Map<String, Object> json = new Gson().fromJson(data, Map.class);
 
-		@SuppressWarnings("unchecked")
-		final Map<String, Object> currentVersionPatches = (Map<String, Object>) json.get(currentModVersion);
+		@SuppressWarnings("unchecked") final Map<String, Object> currentVersionPatches = (Map<String, Object>) json.get(currentModVersion);
 
 		final List<String> sortedCurrentVersionPatches = currentVersionPatches.keySet().stream().sorted().collect(Collectors.toList());
 
@@ -90,28 +167,30 @@ public class DeltaPatcher {
 			final URL patchZipUrl = new URL(patchZipUrlString);
 			final String patchName = StringUtils.reverse(StringUtils.reverse(patchZipUrl.getPath()).split("/")[0]);
 			final File tempPatchZip = Paths.get("./auto-updater/" + modId + "/" + patchName).toFile();
-			downloadZipFile(tempPatchZip, patchZipUrl);
+			try {
+				downloadZipFile(tempPatchZip, patchZipUrl);
+			} catch (IOException e) {
+				//move to next on error
+				continue;
+			}
+			//copy mod file to temp dir and patch
+			break;
 		}
 
 	}
 
-	private static void downloadZipFile(final File outFile, final URL url) {
-		try {
-			URLConnection conn = url.openConnection();
-			InputStream in = conn.getInputStream();
-			FileOutputStream out = new FileOutputStream(outFile);
-			byte[] b = new byte[1024];
-			int count;
-			while ((count = in.read(b)) >= 0) {
-				out.write(b, 0, count);
-			}
-			out.flush();
-			out.close();
-			in.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
+	private static void downloadZipFile(final File outFile, final URL url) throws IOException {
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		FileOutputStream out = new FileOutputStream(outFile);
+		byte[] b = new byte[1024];
+		int count;
+		while ((count = in.read(b)) >= 0) {
+			out.write(b, 0, count);
 		}
+		out.flush();
+		out.close();
+		in.close();
 	}
 
 	/**
